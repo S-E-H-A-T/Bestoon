8367318d9cffb9babde2d838d5e2afdc8735b1b1
Mahdi <sehat.mahdii@gmail.com>
2023-07-30 19:33:23 +0330

    Initial commit

--- web/models.py
+++ web/models.py
@@ -5,6 +5,12 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+class Token(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=48)
+    def __str__(self):
+         return str(self.user)+'_token'
+
 class Expense(models.Model):
     text = models.CharField(max_length=300)
     date = models.DateTimeField()
