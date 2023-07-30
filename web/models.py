7855eeae568e0511661ddbfb22cf5ba93ae2a9d5
Mahdi <sehat.mahdii@gmail.com>
2023-07-30 17:21:47 +0330

    add title to models

--- web/models.py
+++ web/models.py
@@ -10,13 +10,13 @@ class Expense(models.Model):
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    def __unicode__(self):
-        return "{}-{}".format(self.date, self.amount)
+    def __str__(self):
+         return str(self.date)+' -'+str(self.amount)
     
 class Income(models.Model):
     text = models.CharField(max_length=300)
     date = models.DateTimeField()
     amount = models.BigIntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    def __unicode__(self):
-        return "{}-{}".format(self.date, self.amount)
+    def __str__(self):
+         return str(self.date)+' +'+str(self.amount)
