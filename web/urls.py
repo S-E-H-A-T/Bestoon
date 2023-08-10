98ed50ce05fd8dd7ad21405aa8ed0a1c7adc9e8f
Mahdi <sehat.mahdii@gmail.com>
2023-08-10 17:16:58 +0330

    add edit

--- web/urls.py
+++ web/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 urlpatterns = [
     path('submit/expense/', views.submit_expense, name='submit_expense'),
+    path('edit/expense/', views.edit_expense, name='edit_expense'),
     path('submit/income/', views.submit_income, name='submit_income'),
+    path('edit/income/', views.edit_income, name='edit_income'),
     path('q/generalstat/', views.generalstat, name='generalstat'),
     path('q/expenses/', views.query_expenses, name='query_expenses'),
     path('q/incomes/', views.query_incomes, name='query_incomes'),
